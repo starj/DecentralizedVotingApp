e4c180ad0b732c352714da2a19f5036af469ab35
starj <asin489363@gmail.com>
2024-07-26 16:29:15 +0200

    add optimization

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract DecentralizedVotingApp {
@@ -16,16 +17,17 @@ contract DecentralizedVotingApp {
     event VoteCreated(uint id, string description);
     event Voted(uint voteId, uint candidateId, address voter);
 
-    function createVote(string memory _description, uint[] memory _candidateList) public {
+    function createVote(string memory _description, uint[] calldata _candidateList) public {
         require(_candidateList.length > 0, "Candidate list must contain at least one candidate.");
         voteCount++;
         Vote storage newVote = votes[voteCount];
         newVote.id = voteCount;
         newVote.description = _description;
         newVote.exists = true;
+        
         for (uint i = 0; i < _candidateList.length; i++) {
             require(_candidateList[i] != 0, "Candidate ID 0 is reserved and cannot be explicitly used.");
-            newVote.candidateVotes[_candidateList[i]] = 0;
+            newVote.candidateVotes[_candidateList[i]] = 0; 
             newVote.candidateList.push(_candidateList[i]);
         }
 
@@ -47,18 +49,19 @@ contract DecentralizedVotingApp {
         require(votes[_voteId].exists, "Vote does not exist.");
         uint[] memory ids = new uint[](votes[_voteId].candidateList.length);
         uint[] memory voteCounts = new uint[](votes[_voteId].candidateList.length);
-
+        
         for (uint i = 0; i < votes[_voteId].candidateList.length; i++) {
             ids[i] = votes[_voteId].candidateList[i];
             voteCounts[i] = votes[_voteId].candidateVotes[ids[i]];
         }
+        
         return (ids, voteCounts);
     }
     
-    // Helper function to check if a candidate is in a particular vote
-    function _isCandidateInVote(uint _voteId, uint _candidateLexId) private view returns (bool) {
-        for (uint i = 0; i < votes[_voteId].candidateList.length; i++) {
-            if (votes[_voteId].candidateList[i] == _candidateId) {
+    function _isCandidateInPropsosal(uint _voteId, uint _candidateId) private view returns (bool) {
+        uint length = votes[_voteId].candidateList.length;
+        for (uint i = 0; i < length; i++) {
+            if (votes[_voteText>[i] == _candidateId) {
                 return true;
             }
         }
